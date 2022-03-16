52d3bd0b424c9e7ce3b491e344e7cb551f9ca3de
Mattia471 <cucuzzamattia47@gmail.com>
2022-03-16 14:54:04 +0100

    HTTP CLIENT + ROUTING

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TableComponent} from "./table/table.component";
 import {RouterModule, Routes} from "@angular/router";
+import {FinalTable} from "./config-template/table/example-config";
 
 
 const routes: Routes = [
-  {path : 'users', component: TableComponent}
+  {
+    path : 'users',
+    component: TableComponent
+  }
 ];
 
 @NgModule({
